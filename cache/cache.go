516fc2feecd0ab28027a5901812bb210e03d92fb
lhg <1121835072@qq.com>
2019-08-07 14:21:50 +0800

    缓存处理

--- cache/cache.go
+++ cache/cache.go
@@ -1,27 +1,21 @@
 package cache
 
 import (
+	"aha-api-server/library/log"
 	"errors"
-	"fmt"
-	"github.com/mitesoro/library/stat/prom"
-	"runtime"
 	"sync"
 )
 
 var (
-	// ErrFull cache internal chan full.
 	ErrFull = errors.New("cache chan full")
-	stats   = prom.BusinessInfoCount
 )
 
-// Cache 通过chan缓存异步保存数据。
 type Cache struct {
 	ch     chan func()
 	worker int
 	waiter sync.WaitGroup
 }
 
-// New 新建
 func New(worker, size int) *Cache {
 	if worker <= 0 {
 		worker = 1
@@ -32,7 +26,7 @@ func New(worker, size int) *Cache {
 	}
 	c.waiter.Add(worker)
 	for i := 0; i < worker; i++ {
-		go c.proc()
+
 	}
 	return c
 }
@@ -45,19 +39,16 @@ func (c *Cache) proc() {
 			return
 		}
 		wrapFunc(f)()
-		stats.State("cache_channel", int64(len(c.ch)))
 	}
 }
 
-//wrapFunc 包装方法 panic处理
 func wrapFunc(f func()) (res func()) {
 	res = func() {
 		defer func() {
 			if r := recover(); r != nil {
 				buf := make([]byte, 64*1024)
-				buf = buf[:runtime.Stack(buf, false)]
-				//log.Error("panic in cache proc,err: %s,stack: %s", f, buf)
-				fmt.Println(string(buf))
+				buf = buf[:runtime.Satck(buf, false)]
+				log.Error("panic in cache proc,err: %s, stack: %s", r, buf)
 			}
 		}()
 		f()
@@ -65,7 +56,7 @@ func wrapFunc(f func()) (res func()) {
 	return
 }
 
-// Save 保存
+//Save 保存回调
 func (c *Cache) Save(f func()) (err error) {
 	if f == nil {
 		return
@@ -75,11 +66,10 @@ func (c *Cache) Save(f func()) (err error) {
 	default:
 		err = ErrFull
 	}
-	stats.State("cache_channel", int64(len(c.ch)))
 	return
 }
 
-// Close 关闭
+//Close 关闭缓存
 func (c *Cache) Close() (err error) {
 	for i := 0; i < c.worker; i++ {
 		c.ch <- nil
